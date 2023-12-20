6506ae0b36a4f90022e8607df38e5225d2c8ff2d
Discat2 <ragarcia12@espe.edu.ec>
2023-12-20 18:53:47 -0500

    Inicializacion examen

---
+++ src/main/java/ec/edu/espe/examengarcia/ExamengarciaApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.examengarcia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamengarciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamengarciaApplication.class, args);
+	}
+
+}
